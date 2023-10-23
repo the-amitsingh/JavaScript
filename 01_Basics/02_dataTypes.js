e5e4f3df477c52ea1503f79ee270ddec3bdd642c
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-23 19:42:28 +0530

    Datatypes in js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" ; // Treat all js code as newer version 
+
+// alert(3 + 3 ) we are using nodejs not browser 
+
+let name = "Amit"  //String 
+let age = 20  // Number 
+let isLoggedIn = false //Boolean 
+let state ;
+
+//number => 2 to 53
+//bigInt 
+// String => ""
+// null => Standalone value 
+//undefined =>
+//symbol => unique 
+
+
+// object 
+
+
+console.log(typeof  undefined); // undefined 
+console.log(typeof null); // object 
+
