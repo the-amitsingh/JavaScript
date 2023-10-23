d76c1838240699b4f35006dffbc45727e4b7fa33
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-23 20:01:30 +0530

    conbversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score ="29"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// " 33abc" => NaN( Not a number)
+// true => 1; false =>0
+
+let isLoggedIn = "Amit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// " " => false 
+// "Amit" => true 
+
+let someNumber = 23
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
