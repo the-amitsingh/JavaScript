c494784d1811d794b1a6fda86c9a933b814903dc
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-23 20:17:38 +0530

    conversion of datatypes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -24,3 +24,37 @@ let someNumber = 23
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ************************************Operations **********************************
+let value =23
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "Amit "
+let str2 = " Singh "
+console.log(str1 + str2);
+
+console.log( "1" + 2) ;
+console.log( 1+ "2") ;
+console.log( "1" + 2 + 2) ;
+console.log( 1 + 2 + "2") ;
+
+console.log(+true);
+console.log(+"");
+
+// let num1, num2 , num3 
+// num1 = num2 = num3 =2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
