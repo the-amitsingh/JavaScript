c932ed7fc1eb55b5683db8da60e2943a35ee5710
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-27 23:11:12 +0530

    Scope in js

---
+++ 03_Basics/02_Scope.js
@@ -0,0 +1,50 @@
+//  var c =300
+//  let a =100
+
+//  if (true ){
+//     let a = 10
+//     const b = 20 
+//     console.log("Inner :", a);
+
+//  }
+//  console.log(a);
+// //  console.log(b);
+// //  console.log(c);
+
+// //Nested Scope
+
+// function one (){
+//     const username = "Amit"
+
+//     function Two(){
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//    // console.log(website);
+//     Two()
+// }
+// // one()
+
+if (true){
+    const username = "Amit "
+    if(username =="Amit"){
+        const website = "Youtube "
+        console.log(username + website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+
+// ++++++++++++++++++++++++++++++++++++Interesting +++++++++++++++++++++++++++++++
+console.log( addone(5));
+function addone (num){
+    return num+1
+}
+
+
+addTwo(6)
+const addTwo = funtion(num){
+    return  num + 2
+}
+ 
