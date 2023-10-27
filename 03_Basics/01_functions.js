9ace7772aa8aeaa16b0e0233224778150352ef66
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-27 23:11:00 +0530

    functions in js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -18,3 +18,34 @@ function loginUserMessage(username ){
 //console.log(loginUserMessage("hitesh"));
 // if we dont pass any value then it will be undefined 
 console.log(loginUserMessage());
+
+function calculateCartPrice(...num1){
+    return num1
+    // ... are called as rest operator as well as spread operator 
+}
+console.log(calculateCartPrice(222 , 4554 ,845 ))
+
+const user = {
+    username : "Amit",
+    price :244
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+//handleObject(user )
+// We can pass direct object also 
+
+handleObject({
+    username :"Sam ",
+    price : "399"
+})
+
+// we can pass array  also 
+const myNewArray = [200,300,400,5000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));  OR
+console.log(returnSecondValue([200,300,400,5000]));
