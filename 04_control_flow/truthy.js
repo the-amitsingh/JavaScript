305c0a4e2be038412754a918a163fb18920a9e9b
Amit kumar singh <amitsinghdbg2001@gmail.com>
2023-10-30 15:06:37 +0530

    Truthy and falsy in js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -19,3 +19,19 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length ===0){
     console.log("Object is empty ");
 }
+
+//Nullish Coalescing Operator (??) : null undefined 
+
+let val1 ;
+// val1 = 5 ?? 10 
+// val1 = null ?? 10 
+// val1 = undefined ?? 15
+val1 = null ?? 10 ??20 
+
+console.log(val1);
+
+//Terniary operator 
+//condition ?true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("Less than 80"):console.log("more than 80");
